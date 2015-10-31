e50a4e533d0001659a2ff8715329ffa3db4f2d32
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2015-10-31 11:35:27 -0700

    Working front end app

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var ejsLayouts = require("express-ejs-layouts");
+var request = require('request');
+//var db = require('./models');
+
+app.use(ejsLayouts);
+
+app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+	res.render("index");
+});
+
+app.listen(process.env.PORT || 3000);
