b850dcc119c6cb29749e9cf253055e03ae192aae
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2015-11-02 11:50:52 -0800

    Added express sessions

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ var app = express();
 var bodyParser = require('body-parser');
 var ejsLayouts = require("express-ejs-layouts");
 var request = require('request');
+var session = require('express-session');
+
 //var db = require('./models');
 
 app.use(ejsLayouts);
 
 app.use(express.static(__dirname + '/static'));
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({
+  secret: 'wdipingpong',
+  resave: false,
+  saveUninitialized: true
+}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
